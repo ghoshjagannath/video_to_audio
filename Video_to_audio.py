7dc6e65c8cee62e42c9f00fb504af668b19e96c1
Jagannath Ghosh <ghoshjagannath0000@gmail.com>
2022-09-15 22:35:01 +0530

    video t audio file updated

--- Video_to_audio.py
+++ Video_to_audio.py
@@ -1,19 +1,32 @@
 import moviepy.editor
+from playsound import playsound
+
 
 # Replace the parameter with the location of the video
-video = moviepy.editor.VideoFileClip(r"C:/Users/ghosh/Downloads/STEP BY STEP SURYA NAMASKAR FOR BEGINNERS _ Learn Sun Salutation In 3 Minutes_ Simple Yoga Lessons.mp4")
+video = moviepy.editor.VideoFileClip(r"C:/Users/ghosh/Downloads/studyiq.mp4")
 
 video.audio.write_audiofile("C:/Users/ghosh/some.wav")
 
 
-import speech_recognition as sr
-from translate import Translator
-translator= Translator(to_lang="bn")
 
-
-
-# # transcribe audio file                                                         
+# # transcribe audio file                                                       
 AUDIO_FILE ="C:/Users/ghosh/some.wav"
+
+##Playsound module of 1.2.2 version only able to play music in wav format  
+# playsound(AUDIO_FILE)
+
+
+
+
+
+
+import speech_recognition as sr
+# from translate import Translator
+# translator= Translator(to_lang="bn")
+
+
+
+
 
 
 
@@ -21,6 +34,11 @@ AUDIO_FILE ="C:/Users/ghosh/some.wav"
 #use the audio file as the audio source                                        
 r = sr.Recognizer()
 with sr.AudioFile(AUDIO_FILE) as source:
-        audio = r.record(source)  # read the entire audio file                  
-        texts= r.recognize_google(audio)
+        # read the entire audio file    
+        audio = r.record(source)               
+        texts= r.recognize_google(audio,language="en-US")
         print(texts)
+        
+        
+        
+        
