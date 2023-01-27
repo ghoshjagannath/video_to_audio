f3c5442b57458c05af7de197ee9b7933587ade67
Jagannath Ghosh <ghoshjagannath0000@gmail.com>
2023-01-27 07:21:26 +0530

    photo is deleted

--- Video_to_audio.py
+++ Video_to_audio.py
@@ -14,21 +14,9 @@ AUDIO_FILE ="C:/Users/ghosh/some.wav"
 
 ##Playsound module of 1.2.2 version only able to play music in wav format  
 # playsound(AUDIO_FILE)
-
-
-
-
-
-
 import speech_recognition as sr
 # from translate import Translator
 # translator= Translator(to_lang="bn")
-
-
-
-
-
-
 
 
 #use the audio file as the audio source                                        
